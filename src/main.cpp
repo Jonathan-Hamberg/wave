d51da05594ae4b285bc6b119e6d1cd984c491e79
Jonathan Hamberg <jonathanhamberg@gmail.com>
2017-02-24 12:19:11 -0800

    Now generating project files with CMake. Initial Commit with the IMGUI demo code working.

---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <glad/glad.h>
+#include "GLFW/glfw3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+
+
+
+void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+
+}
+
+void glfw_error_callback(int error, const char *description)
+{
+    std::cout << "error code: " << error << " " << description << std::endl;
+}
+
+int main(int argv, char *argc[])
+{
+    GLFWwindow *glfwWindow = nullptr;
+
+    glfwInit();
+
+    glfwSetErrorCallback(glfw_error_callback);
+
+    glfwWindow = glfwCreateWindow(640, 480, "Wave", nullptr, nullptr);
+    glfwMakeContextCurrent(glfwWindow);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    glfwSetKeyCallback(glfwWindow, glfw_key_callback);
+
+    ImGui_ImplGlfwGL3_Init(glfwWindow, true);
+
+    bool show_test_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImColor(114, 144, 154);
+   
+    while (!glfwWindowShouldClose(glfwWindow)) {
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(glfwWindow, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        glfwSwapBuffers(glfwWindow);
+    }
+
+    glfwTerminate();
+}
